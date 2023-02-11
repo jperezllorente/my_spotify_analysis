c6bdea05d888be0bc5fccc35af105258d349dc42
Juan Perez <jperezllorente@hotmail.com>
2023-02-11 12:44:02 +0100

    changes

---
+++ secrets.py
@@ -0,0 +1,4 @@
+secrets = {'SPOTIPY_CLIENT_ID':'bee50ef8746e41e293d2b3eaabc012dd',
+'SPOTIPY_CLIENT_SECRET':'a13911965b1246959864149c52a26749',
+'SPOTIPY_REDIRECT_URI' : "https://jperezllo.con/callback/",
+'username' : 'perezllo'}
