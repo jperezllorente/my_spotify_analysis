8996a2081cc3434d855ba9d44f387dfeeba4f289
Juan Perez <jperezllorente@hotmail.com>
2023-02-12 19:05:00 +0100

    artists info dataset created

--- credential.py
+++ credential.py
@@ -2,3 +2,8 @@ secrets = {'SPOTIPY_CLIENT_ID':'bee50ef8746e41e293d2b3eaabc012dd',
 'SPOTIPY_CLIENT_SECRET':'a13911965b1246959864149c52a26749',
 'SPOTIPY_REDIRECT_URI' : "https://jperezllo.con/callback/",
 'username' : 'perezllo'}
+
+secrets_2 = {'SPOTIPY_CLIENT_ID':'554f5fa49de24e619b42ce25108d2a37',
+'SPOTIPY_CLIENT_SECRET':'5653e5a938824e5abb69d218cc1a5075',
+'SPOTIPY_REDIRECT_URI' : "https://mimusica.com/callback/",
+'username' : 'ellorentep'}
